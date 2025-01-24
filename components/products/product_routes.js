8ab376c5a7cb55a328d9d78baafdbbf5dee525cb
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-24 20:39:37 +0530

    components: product[routes + validation]

---
+++ components/products/product_routes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import * as product from './product_controller.js';
+import { validate } from '../../middleware/validation.js';
+import {
+    addProductValidation,
+    deleteProductValidation,
+    getSpecificProductValidation,
+    updateProductValidation,
+} from "./product_validation.js";
+import { uploadMultipleFiles } from '../../db/multer.js';
+import { allowedTo, protectedRoutes } from '../auth/auth_controller.js';
+
+
+const productRouter = express.Router();
+
+let arrFields = [
+    { name: "imgCover", maxCount: 1 },
+    { name: "images", maxCount: 20 },
+];
+
+productRouter.route("/").post(protectedRoutes, allowedTo("admin", "user"), uploadMultipleFiles(arrFields, "products"), validate(addProductValidation), product.addProduct).get(product.getAllProducts);
+
+productRouter.route("/:id").put(protectedRoutes, allowedTo("admin"), validate(updateProductValidation), product.updateProduct).delete(protectedRoutes, allowedTo("admin"), validate(deleteProductValidation), product.deleteProduct).get(validate(getSpecificProductValidation), product.getSpecificProduct);
+
+export default productRouter;
