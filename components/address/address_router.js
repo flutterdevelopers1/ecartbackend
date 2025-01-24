67db0f287027f6c5d4267fe548ca443634980500
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-24 20:18:05 +0530

    components: address[address_router]

--- components/address/address_router.js
+++ components/address/address_router.js
@@ -1,19 +1,14 @@
 import express from "express";
 import { validate } from "../../middlewares/validate.js";
-import { allowedTo, protectedRoutes } from "../auth/auth.controller.js";
-import * as address from "../address/address.controller.js";
+import { allowedTo, protectedRoutes } from "../auth/auth_controller.js";
+import * as address from "../address/address_controller.js";
 import {
     addAddressValidation,
     deleteAddressValidation,
-} from "./address.validation.js";
+} from "./address_validation.js";
 
 const addressRouter = express.Router();
 
-addressRouter.route("/").patch(protectedRoutes, allowedTo("user"), validate(addAddressValidation), address.addAddress).delete(protectedRoutes,
-    allowedTo("user"),
-    validate(deleteAddressValidation),
-    address.removeAddress
-)
-    .get(protectedRoutes, allowedTo("user"), address.getAllAddresses);
+addressRouter.route("/").patch(protectedRoutes, allowedTo("user"), validate(addAddressValidation), address.addAddress).delete(protectedRoutes, allowedTo("user"), validate(deleteAddressValidation), address.removeAddress).get(protectedRoutes, allowedTo("user"), address.getAllAddresses);
 
 export default addressRouter;
