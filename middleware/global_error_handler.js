40aadc47aeab75048ce299bd8f37bd0a6c2689d1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-24 20:01:16 +0530

    middleware: global error handler + validator

---
+++ middleware/global_error_handler.js
@@ -0,0 +1,7 @@
+export const globalErrorHandler = (err, req, res, next) => {
+    let error = err.message;
+    let code = err.statuscode || 500;
+    process.env.MODE == "dev"
+        ? res.status(code).json({ error, stack: err.stack })
+        : res.status(code).json({ error });
+};
